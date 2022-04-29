356b656fa108bee7eddc657efc95266480c418ad
Leke Akinsanya <aakinsanya@aakinsanya-a01.vmware.com>
2022-04-28 20:50:01 -0400

    first

---
+++ FooService.Tests/WeatherForecastTests.cs
@@ -0,0 +1,28 @@
+using NUnit.Framework;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using foo.Controllers;
+
+namespace FooService.Tests;
+
+public class WeatherForecastTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        //Arrange
+        var logger = new Logger<WeatherForecastController>(new NullLoggerFactory());
+        var controller = new WeatherForecastController(logger);
+        
+        //Act
+        var result = controller.Get();
+        
+        //Assert
+        Assert.AreEqual(result.Count, 5);
+    }
+}
---
+++ FooService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace foo.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<WeatherForecastController> _logger;
+
+    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetWeatherForecast")]
+    public IList<WeatherForecast> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToList();
+    }
+}
+
+
+
+[ApiController]
+//[Route("[controller]")]
+public class HomeController : ControllerBase
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    [Route("")]
+    [Route("Home")]
+    [Route("Home/Index")]
+    [Route("Home/Index/{id?}")]
+    public ActionResult Index()
+    {
+        return Ok("Works!");
+    }
+}
